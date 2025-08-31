ca1cb9b3a4b44f4bc02073821a618ecfc53da34a
Aom <Aaom6740@gmail.com>
2025-08-31 20:36:15 +0700

    First commit

---
+++ 3-1.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")(); 
+let score = parseInt(prompt("กรุณากรอกคะแนน (0-100): "));
+if (score >= 80 && score <= 100) {
+  console.log("เกรด A");
+}
+if (score >= 70 && score <= 79) {
+  console.log("เกรด B");
+}
+if (score >= 60 && score <= 69) {
+  console.log("เกรด C");
+}
+if (score >= 50 && score <= 59) {
+  console.log("เกรด D");
+}
+if (score < 50) {
+  console.log("เกรด F");
+}
